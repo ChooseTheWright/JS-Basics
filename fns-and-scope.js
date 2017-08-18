22a2977ef35083bc0ace3c71d5dda6713eded037
Wright <koltw@ogdencity.com>
2017-08-17 20:15:39 -0600

    I added a solution to problem 7.

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -86,9 +86,14 @@ alert(newMyName);
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
   //Code Here
-
+function outerFn () {
+  function () {
+    return newMyName;
+  }
+}
 //Now save the result of invoking outerFn into a variable called innerFn.
 
   //Code Here
-
+var innerFn = outerFn();
 //Now invoke innerFn.
+innerFn();
